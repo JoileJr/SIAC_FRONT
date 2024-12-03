810472d13dcbab5856b1c0aa195de85493ad1048
Joíle Júnior <105524292+JoileJr@users.noreply.github.com>
2024-12-03 11:35:26 -0300

    Menus exibidos dependendo da role

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit, WritableSignal, signal } from "@angular/core";
-import { AuthenticationService } from "../../core/services/authentication/authentication.service";
-import { Router } from "@angular/router";
-import { RoutesConstants } from "../../core/constants/routes.constants";
-import { MenuItem, MessageService } from "primeng/api";
+import {ChangeDetectionStrategy, Component, OnInit, WritableSignal, signal} from "@angular/core";
+import {AuthenticationService} from "../../core/services/authentication/authentication.service";
+import {Router} from "@angular/router";
+import {RoutesConstants} from "../../core/constants/routes.constants";
+import {MenuItem, MessageService} from "primeng/api";
 
 @Component({
     selector: 'app-topbar',
@@ -13,14 +13,24 @@ import { MenuItem, MessageService } from "primeng/api";
 export class TopbarComponent implements OnInit {
     routesConstants = RoutesConstants;
     items: MenuItem[] | undefined;
+    user = this.authenticationService.user;
 
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService,
         private messageService: MessageService
-    ) {}
+    ) {
+    }
 
     ngOnInit() {
+        if (this.user?.perfis[0].id == 1) {
+            this.menusAdmin();
+        } else {
+            this.menusPac();
+        }
+    }
+
+    menusAdmin() {
         this.items = [
             {
                 label: 'Inicio',
@@ -48,6 +58,18 @@ export class TopbarComponent implements OnInit {
                 icon: 'pi pi-users',
                 command: () => {
                     this.redirectRoute(this.routesConstants.HEALTH_PROFESSIONAL);
+                }
+            }
+        ];
+    }
+
+    menusPac() {
+        this.items = [
+            {
+                label: 'Inicio',
+                icon: 'pi pi-home',
+                command: () => {
+                    this.redirectRoute(this.routesConstants.HOME);
                 }
             }
         ];
